99385d9e8ee13baaf8bfd32d3c00656073d2340c
Angie Ann Camilleri <angieanncamilleri@yahoo.com>
2016-04-11 13:33:26 +0200

    Initial startup test. Test is irrelevant to the project. Purpose of this test is to make sure that project and junit are working accordingly.

--- src/test/java/uom/edu/se_assignment/LibrarySystem/BookTest.java
+++ src/test/java/uom/edu/se_assignment/LibrarySystem/BookTest.java
@@ -1,10 +1,23 @@
 package uom.edu.se_assignment.LibrarySystem;
 
-import junit.framework.TestCase;
+/*import junit.framework.TestCase;
 import junit.framework.Test;
-import junit.framework.TestSuite;
+import junit.framework.TestSuite;*/
 
+import org.junit.*;
 
-public class BookTest extends TestCase {
-
+public class BookTest /*extends TestCase*/ {
+	
+	@Before 
+	public void startup() 
+	{
+		//
+	}
+	
+	@org.junit.Test
+	public void test1()
+	{
+		Assert.assertTrue(true);
+	}
+	
 }
