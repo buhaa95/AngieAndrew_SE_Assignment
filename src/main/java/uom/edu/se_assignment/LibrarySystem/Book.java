6a1cc230666ab43f10ebbdff7fbdd2d7fa5742c5
Angie Ann Camilleri <angieanncamilleri@yahoo.com>
2016-04-11 14:36:15 +0200

    Initial Failing Tests for Book class

--- src/main/java/uom/edu/se_assignment/LibrarySystem/Book.java
+++ src/main/java/uom/edu/se_assignment/LibrarySystem/Book.java
@@ -1,6 +1,65 @@
 package uom.edu.se_assignment.LibrarySystem;
 
+import java.util.Date;
+
 public class Book {
 	
+	private String Title;
+	private String Author;
+	//TODO Genre should be changed to genre object
+	private String Genre;
+	private int PublicationYear;
+	private int Edition;
+	private User Loanee;
+	private String LoanOutDate;
+	
+	Book(String t, String a, String g, int y, int e, User l, String d)
+	{
+		Title = t;
+		Author = a;
+		Genre = g;
+		PublicationYear = y;
+		Edition = e;
+		Loanee = l;
+		LoanOutDate = d;
+	}
 
+	public String getTitle() {
+		
+		return null;
+	}
+	
+	public String getAuthor()
+	{
+		return null;
+	}
+	
+	public String getGenre()
+	{
+		return null;
+	}
+	
+	public int getYear()
+	{
+		return 0;
+	}
+	
+	public int getEdition()
+	{
+		return 0;
+		
+	}
+	
+	public User getLoanee()
+	{
+		return null;
+	}
+	
+	public String getLoanOutDate()
+	{
+		return null;
+		
+	}
+	
+	
 }
--- src/test/java/uom/edu/se_assignment/LibrarySystem/BookTest.java
+++ src/test/java/uom/edu/se_assignment/LibrarySystem/BookTest.java
@@ -8,16 +8,52 @@ import org.junit.*;
 
 public class BookTest /*extends TestCase*/ {
 	
+	Book myBook;
 	@Before 
 	public void startup() 
 	{
-		//
+		myBook = new Book("Harry Potter and the Half Blood Prince", "J.K. Rowling", "Fiction", 2008,1, null, "21/12/2012" );
 	}
 	
 	@org.junit.Test
-	public void test1()
+	public void getTitleTest()
 	{
-		Assert.assertTrue(true);
+		Assert.assertEquals("Harry Potter and the Half Blood Prince", myBook.getTitle());
 	}
 	
+	@org.junit.Test
+	public void getAuthorTest()
+	{
+		Assert.assertEquals("J.K. Rowling", myBook.getAuthor());
+	}
+	
+	@org.junit.Test
+	public void getYearTest()
+	{
+		Assert.assertEquals(2008, myBook.getYear());
+	}
+	
+	@org.junit.Test
+	public void getGenreTest()
+	{
+		Assert.assertEquals("Fiction", myBook.getGenre());
+	}
+	
+	@org.junit.Test
+	public void getEditionTest()
+	{
+		Assert.assertEquals(1, myBook.getEdition());
+	}
+	
+	@org.junit.Test
+	public void getUserTest()
+	{
+		Assert.assertEquals(null, myBook.getLoanee());
+	}
+	
+	@org.junit.Test
+	public void getLoanOutDateTest()
+	{
+		Assert.assertEquals("21/12/2012", myBook.getLoanOutDate());
+	}
 }
