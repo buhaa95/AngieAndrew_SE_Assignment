681b3cf6ec89b645b7b91a5b14888cfc6890b3af
Angie Ann Camilleri <angieanncamilleri@yahoo.com>
2016-04-12 10:15:16 +0200

    Genre class with GenreTest class with working tests

---
+++ src/main/java/uom/edu/se_assignment/LibrarySystem/Genre.java
@@ -0,0 +1,22 @@
+package uom.edu.se_assignment.LibrarySystem;
+
+
+public class Genre 
+{	
+	String genreType;
+	
+	public Genre(String genreType) 
+	{
+		this.genreType = genreType;
+	}
+
+	public String getGenreType() 
+	{
+		return genreType;
+	}
+
+	public void setGenreType(String genreType) 
+	{
+		this.genreType = genreType;
+	}
+}
