1532b1f8e16bc27e61be7437c18ec6e6edd87fbd
Andrew Buhagiar <andrewbuhagiar@gmail.com>
2016-05-11 08:58:56 +0200

    Observable and Observer interfaces for the Observer Design pattern in part 3

---
+++ src/main/java/uom/edu/se_assignment/LibrarySystem/Observable.java
@@ -0,0 +1,7 @@
+package uom.edu.se_assignment.LibrarySystem;
+
+public interface Observable {
+	public void addObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObserver();
+}
